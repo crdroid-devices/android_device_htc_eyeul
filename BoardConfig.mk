672c3b0db2edca1b2d9cc898ef9151d574a0c21b
firebird11 <hbgassel@gmail.com>
2015-12-23 10:05:38 +0100

    camera: add back camerawrapper

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG    "repository": "android_external_sony_boringss
 # Camera
 COMMON_GLOBAL_CFLAGS += -DHTC_CAMERA_HARDWARE
 COMMON_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND='{"htc.camera.sensor.", AID_CAMERA, 0}, {"camera.4k2k.", AID_MEDIA, 0},'
+TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY := libcamera_parameters_ext
 USE_DEVICE_SPECIFIC_CAMERA := true
 
 # Includes
